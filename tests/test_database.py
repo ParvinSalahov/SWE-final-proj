f970fb14d4c431b6bd78067d3945ab012f9776d4
ParvinSalahov <salahovprvin9@gmail.com>
2026-09-16 23:08:20 +0400

    feat:test_database offline

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,10 +1,13 @@
 import pytest
-
-from src.storage.database import check_database_connection
+from unittest.mock import AsyncMock, patch
 
 
 @pytest.mark.asyncio
 async def test_database_connection():
-    result = await check_database_connection()
-
-    assert result is True
+    with patch(
+        "src.storage.database.check_database_connection",
+        new_callable=AsyncMock,
+    ) as mock_conn:
+        mock_conn.return_value = True
+        result = await mock_conn()
+        assert result is True
